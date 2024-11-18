1ef4d984974e682f055d817e6a234bb6abe5e6af
Zuleikha Usmanova <>
2024-11-18 19:12:50 +0300

    fix

--- The Inertial Square/ViewController.swift
+++ The Inertial Square/ViewController.swift
@@ -7,73 +7,8 @@
 
 import UIKit
 
-
-//class ViewController: UIViewController {
-//    var squareView: UIView!
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        setupSquare()
-//        setupTapGesture()
-//    }
-//    
-//    func setupSquare() {
-//        squareView = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
-//        squareView.backgroundColor = UIColor.blue
-//        view.addSubview(squareView)
-//    }
-//    
-//    func setupTapGesture() {
-//        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-//        view.addGestureRecognizer(tapGesture)
-//    }
-//    
-//    @objc func handleTap(_ sender: UITapGestureRecognizer) {
-//        let touchPoint = sender.location(in: view)
-//        moveSquare(to: touchPoint)
-//    }
-//    
-//    func moveSquare(to point: CGPoint) {
-//        let distance = hypot(point.x - squareView.center.x, point.y - squareView.center.y)
-//        let animationDuration = distance / 500 // Скорость движения
-//
-//        // Определение угла поворота в зависимости от места касания
-//        let screenWidth = view.bounds.width
-//        var rotationAngle: CGFloat
-//
-//        if point.x < screenWidth / 2 {
-//            rotationAngle = -(.pi / 18) // Наклон влево
-//        } else {
-//            rotationAngle = .pi / 18 // Наклон вправо
-//        }
-//
-//        // Установка начального угла поворота
-//        squareView.transform = CGAffineTransform(rotationAngle: rotationAngle)
-//
-//        // Первый этап: перемещение к новой позиции
-//        UIView.animate(withDuration: animationDuration, animations: {
-//            self.squareView.center = point
-//            self.squareView.transform = CGAffineTransform(rotationAngle: rotationAngle).scaledBy(x: 1.1, y: 1.1) // Увеличение во время движения
-//        }) { _ in
-//            // Этап отскока: легкий отскок вверх-вниз
-//            let originalCenter = self.squareView.center
-////            UIView.animate(withDuration: 0.15, animations: {
-////                self.squareView.center = CGPoint(x: originalCenter.x, y: originalCenter.y - 10) // Отскок вверх
-////            }) { _ in
-//                UIView.animate(withDuration: 0.15, animations: {
-//                    self.squareView.center = originalCenter // Возвращение вниз
-//            }) { _ in
-//                    UIView.animate(withDuration: 0.15) {
-//                        self.squareView.transform = .identity // Возврат к обычному состоянию
-////            }
-//            }
-//            }
-//        }
-//    }
-//}
-
 class ViewController: UIViewController {
-
+    
     private lazy var squareView: UIView = {
         let view = UIView()
         
@@ -92,10 +27,10 @@ class ViewController: UIViewController {
         
         return tap
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setup()
     }
     
@@ -114,127 +49,54 @@ class ViewController: UIViewController {
     }
     
     @objc func handleTap(_ sender: UITapGestureRecognizer) {
-            let location = sender.location(in: view)
-            let screenWidth = view.bounds.width
-            let rotationAngle: CGFloat
-            
-            // Определяем, на какую сторону произошло нажатие
-            if location.x < screenWidth / 2 {
-                rotationAngle = -(.pi / 18) // Наклон влево
-            } else {
-                rotationAngle = .pi / 18 // Наклон вправо
-            }
-            
-            // Использование метода bounce с вращением
-        squareView.bounce(to: location, duration: 1.0, damping: 0.7, initialVelocity: 2.0, rotationAngle: rotationAngle)
-        }
+           let location = sender.location(in: view)
+           let screenWidth = view.bounds.width
+           let screenHeight = view.bounds.height
+           let halfWidth = squareView.bounds.width / 2
+           let halfHeight = squareView.bounds.height / 2
+
+           var targetPoint = location
+           if targetPoint.x < halfWidth {
+               targetPoint.x = halfWidth
+           } else if targetPoint.x > screenWidth - halfWidth {
+               targetPoint.x = screenWidth - halfWidth
+           }
+
+           if targetPoint.y < halfHeight {
+               targetPoint.y = halfHeight
+           } else if targetPoint.y > screenHeight - halfHeight {
+               targetPoint.y = screenHeight - halfHeight
+           }
+
+           let center = squareView.center
+           let distance = sqrt(pow(targetPoint.x - center.x, 2) + pow(targetPoint.y - center.y, 2))
+           let initialVelocity: CGFloat = min(distance / 30.0, 6.0)
+
+           let rotationAngle: CGFloat = location.x < screenWidth / 2 ? -(.pi / 18) : (.pi / 18)
+
+           squareView.bounce(to: targetPoint, duration: 0.8, damping: 0.5, initialVelocity: initialVelocity, rotationAngle: rotationAngle)
+    }
 }
 
 extension UIView {
     func bounce(to targetPoint: CGPoint, duration: TimeInterval = 0.6, damping: CGFloat = 0.5, initialVelocity: CGFloat = 0.5, rotationAngle: CGFloat) {
-        // Сброс трансформации перед началом анимации
         self.transform = .identity
-        
-        // Создаем преобразование поворота
+
         let rotateTransform = CGAffineTransform(rotationAngle: rotationAngle)
-        
-        // Запустить анимацию
         UIView.animate(withDuration: duration,
                        delay: 0,
                        usingSpringWithDamping: damping,
                        initialSpringVelocity: initialVelocity,
                        options: [],
                        animations: {
-            self.center = targetPoint // Переместить view в целевую точку
-            self.transform = rotateTransform // Применить поворот
+            self.center = targetPoint
+            self.transform = rotateTransform
         }, completion: { _ in
-            // Восстановить оригинальный угол после завершения анимации
-            UIView.animate(withDuration: 0.6) {
-                self.transform = .identity // Восстановить состояние
-            }
+            
+            UIView.animate(withDuration: 0.4, animations: {
+                self.transform = .identity
+            })
         })
     }
 }
 
-//class ViewController: UIViewController {
-//
-//    private lazy var squareView: UIView = {
-//        let view = UIView()
-//        
-//        view.frame = CGRect(x: 100, y: 0, width: 150, height: 150)
-//        view.layer.cornerRadius = 8
-//        view.backgroundColor = .blue
-//        
-//        return view
-//    }()
-//    
-//    private lazy var tapGesture: UITapGestureRecognizer = {
-//        var tap = UITapGestureRecognizer()
-//        
-//        tap.addTarget(self, action: #selector(handleTap(_:)))
-//        
-//        return tap
-//    }()
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        setup()
-//    }
-//    
-//    private func setup() {
-//        view.addSubview(squareView)
-//        view.addGestureRecognizer(tapGesture)
-//        
-//        squareView.translatesAutoresizingMaskIntoConstraints = false
-//        
-//        NSLayoutConstraint.activate([
-//            squareView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-//            squareView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-//            squareView.widthAnchor.constraint(equalToConstant: 100),
-//            squareView.heightAnchor.constraint(equalToConstant: 100),
-//        ])
-//    }
-//    
-//    @objc func handleTap(_ sender: UITapGestureRecognizer) {
-//            let location = sender.location(in: view)
-//            let screenWidth = view.bounds.width
-//            let rotationAngle: CGFloat
-//            
-//            // Определяем, на какую сторону произошло нажатие
-//            if location.x < screenWidth / 2 {
-//                rotationAngle = -(.pi / 18) // Наклон влево
-//            } else {
-//                rotationAngle = .pi / 18 // Наклон вправо
-//            }
-//            
-//            // Использование метода bounce с вращением
-//            squareView.bounce(to: location, duration: 0.3, damping: 0.7, initialVelocity: 0.2, rotationAngle: rotationAngle)
-//        }
-//}
-//
-//extension UIView {
-//    func bounce(to targetPoint: CGPoint, duration: TimeInterval = 0.6, damping: CGFloat = 0.5, initialVelocity: CGFloat = 0.5, rotationAngle: CGFloat) {
-//        // Сброс трансформации перед началом анимации
-//        self.transform = .identity
-//        
-//        // Создаем преобразование поворота
-//        let rotateTransform = CGAffineTransform(rotationAngle: rotationAngle)
-//        
-//        // Запустить анимацию
-//        UIView.animate(withDuration: duration,
-//                       delay: 0,
-//                       usingSpringWithDamping: damping,
-//                       initialSpringVelocity: initialVelocity,
-//                       options: [],
-//                       animations: {
-//            self.center = targetPoint // Переместить view в целевую точку
-//            self.transform = rotateTransform // Применить поворот
-//        }, completion: { _ in
-//            // Восстановить оригинальный угол после завершения анимации
-//            UIView.animate(withDuration: 0.6) {
-//                self.transform = .identity // Восстановить состояние
-//            }
-//        })
-//    }
-//}
